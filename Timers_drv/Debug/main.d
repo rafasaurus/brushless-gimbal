e85632e63fe63ec0001a884ac34f431d90ea52c1
rafael2904 <rafa.grigorian@gmail.com>
2017-07-13 11:25:53 +0400

    Add files via upload

--- Timers_drv/Debug/main.d
+++ Timers_drv/Debug/main.d
@@ -1,5 +1,4 @@
-functions.d functions.o: .././functions.cpp .././defines.h \
- .././functions.h \
+main.d main.o: .././main.cpp .././defines.h .././functions.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\sfr_defs.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h \
