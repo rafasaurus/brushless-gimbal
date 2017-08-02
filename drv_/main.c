e8399d71a7cadd44247edfc6dffc44ad3f2b2fa1
Rafael Grigorian <rafa.grigorian@gmail.com>
2017-08-02 17:50:10 +0400

    gyro debug

--- drv_/main.c
+++ drv_/main.c
@@ -467,104 +467,107 @@ int main(void)
 			printf("\n");
 			/*--------end------*/			
 		#else
+			double dt = (double)((micros() - timer1));
+			timer1=micros();
+			double hz=1000000/dt;
+			double gyroXrate = gyro_x  / 65.5 / hz; // Convert to deg/s
+			double gyroYrate = gyro_y  / 65.5 / hz; // Convert to deg/s
 			
-				
-				double dt = (double)((micros() - timer1));
-				timer1=micros();
-				
-				double gyroXrate = gyro_x / 65.5; // Convert to deg/s
-				double gyroYrate = gyro_x / 65.5; // Convert to deg/s
-				
-				angle_pitch += gyro_x*dt/1000000; //Calculate the traveled pitch angle and add this to the angle_pitch variable
-				angle_roll += gyro_y*dt/1000000;  //Calculate the traveled roll angle and add this to the angle_roll variable
-				
-				//0.000001066 = 0.0000611 * (3.142(PI) / 180degr) The Arduino sin function is in radians
-				angle_pitch += angle_roll * sin(gyro_z * 0.000001066);               //If the IMU has yawed transfer the roll angle to the pitch angel
-				angle_roll -= angle_pitch * sin(gyro_z * 0.000001066);               //If the IMU has yawed transfer the pitch angle to the roll angel
-				
-				///double temporar_accel_x=accel_x/100;
-				///double temporar_accel_y=accel_y/100;
-				///double temporar_accel_z=accel_z/100;
-				///
-				///acc_total_vector = sqrt((temporar_accel_x*temporar_accel_x)+(temporar_accel_y*temporar_accel_y)+(temporar_accel_z*temporar_accel_z));  //Calculate the total accelerometer vector
-				/////57.296 = 1 / (3.142 / 180) The Arduino asin function is in radians
-				///acc_total_vector*=100;
-				///angle_pitch_acc = asin((float)accel_y/acc_total_vector)* 57.296;       //Calculate the pitch angle
-				///angle_roll_acc = asin((float)accel_x/acc_total_vector)* -57.296;       //Calculate the roll angle
-				
-				uint16_t reg=angle_pitch;
+			angle_pitch += gyroXrate; //Calculate the traveled pitch angle and add this to the angle_pitch variable
+			angle_roll += gyroYrate;  //Calculate the traveled roll angle and add this to the angle_roll variable
+			
+			//		0.000001066 = 0.0000611 * (3.142(PI) / 180degr) The Arduino sin function is in radians
+			//angle_pitch += angle_roll * sin(gyro_z * 0.000001066);               //If the IMU has yawed transfer the roll angle to the pitch angel
+			//angle_roll -= angle_pitch * sin(gyro_z * 0.000001066);               //If the IMU has yawed transfer the pitch angle to the roll angel
+			//
+			//double temporar_accel_x=accel_x/100;
+			//double temporar_accel_y=accel_y/100;
+			//double temporar_accel_z=accel_z/100;
+			//
+			//acc_total_vector = sqrt((temporar_accel_x*temporar_accel_x)+(temporar_accel_y*temporar_accel_y)+(temporar_accel_z*temporar_accel_z));  //Calculate the total accelerometer vector
+			//		57.296 = 1 / (3.142 / 180) The Arduino asin function is in radians
+			//acc_total_vector*=100;
+			//angle_pitch_acc = asin((float)accel_y/acc_total_vector)* 57.296;       //Calculate the pitch angle
+			//angle_roll_acc = asin((float)accel_x/acc_total_vector)* -57.296;       //Calculate the roll angle
+			
+			uint16_t reg=angle_pitch;
+			printf(" ");
+			printf("gyrox_angle= ");
+			print16(&reg);
+			reg=angle_roll;
+			printf(" ");
+			printf("gyroy_angle= ");
+			print16(&reg);
+			
+			/*double final_angle=angle_roll*0.96+angle_roll_acc*0.04;
+			reg=final_angle;
+			printf(" ");
+			printf("final_angleY= ");
+			print16(&reg);*/
+			
+			reg=micros()-timer1;
+			reg=dt;
+			printf(" ");
+			printf("hz= ");
+			print16ln(&reg);
+			
+			
+			//reg=angle_roll_acc;
+			//printf(" ");
+			//printf("accel_angle= ");
+			//print16ln(&reg);	
+			
+						
+			#ifdef DRV8313
+				double final_angle=angle_roll*0.96+angle_roll_acc*0.04;
+				uint16_t reg_print=final_angle;
 				printf(" ");
-				printf("gyrox_angle= ");
-				print16(&reg);
-				reg=angle_roll;
+				printf("angle_y= ");
+				print16(&reg_print);
 				printf(" ");
-				printf("gyroy_angle= ");
-				print16(&reg);
-				reg=dt;
-				printf(" ");
-				printf("gyroy_angle= ");
-				print16ln(&reg);
-				
-				
-				//reg=angle_roll_acc;
-				//printf(" ");
-				//printf("accel_angle= ");
-				//print16ln(&reg);	
-				
-							
-				#ifdef DRV8313
-					double final_angle=angle_roll*0.96+angle_roll_acc*0.04;
-					uint16_t reg_print=final_angle;
-					printf(" ");
-					printf("angle_y= ");
-					print16(&reg_print);
-					printf(" ");
-					uint16_t learing_rate;
-					int absoulute_y=abs(final_angle);
-					uint16_t local_motor_delay=(32735-(absoulute_y*learing_rate));
-					if (local_motor_delay>2000)
-					{
-						pwm_delay=local_motor_delay;
-					}
-					uint16_t reg_ = local_motor_delay;
-					printf("ocr= ");
-					print16(&reg_);
-					if (reg==0)
-					{
-						incr=0;
-						printf("\n");	
-					}
-					else if (reg<0)
-						{
-							cli();
-							incr=1;
-							printf(" yes\n");
-							
-							sei();
-						}
-						else
-						{	cli();
-							incr=-1;
-							printf(" no\n");
-							sei();
-						}					
-				#endif	//DRV8313
-							 
-				if(set_gyro_angles){                                                 //If the IMU is already started
-					 angle_pitch = angle_pitch * 0.9996 + angle_pitch_acc * 0.0004;     //Correct the drift of the gyro pitch angle with the accelerometer pitch angle
-					 angle_roll = angle_roll * 0.9996 + angle_roll_acc * 0.0004;        //Correct the drift of the gyro roll angle with the accelerometer roll angle
+				uint16_t learing_rate;
+				int absoulute_y=abs(final_angle);
+				uint16_t local_motor_delay=(32735-(absoulute_y*learing_rate));
+				if (local_motor_delay>2000)
+				{
+					pwm_delay=local_motor_delay;
 				}
-				else{                                                                //At first start
-					 angle_pitch = angle_pitch_acc;                                     //Set the gyro pitch angle equal to the accelerometer pitch angle
-					 angle_roll = angle_roll_acc;                                       //Set the gyro roll angle equal to the accelerometer roll angle
-					 set_gyro_angles = true;                                            //Set the IMU started flag
+				uint16_t reg_ = local_motor_delay;
+				printf("ocr= ");
+				print16(&reg_);
+				if (reg==0)
+				{
+					incr=0;
+					printf("\n");	
 				}
+				else if (reg<0)
+					{
+						cli();
+						incr=1;
+						printf(" yes\n");
+						
+						sei();
+					}
+					else
+					{	cli();
+						incr=-1;
+						printf(" no\n");
+						sei();
+					}					
+			#endif	//DRV8313
+						 
+			if(set_gyro_angles){                                                 //If the IMU is already started
+				 angle_pitch = angle_pitch * 0.9996 + angle_pitch_acc * 0.0004;     //Correct the drift of the gyro pitch angle with the accelerometer pitch angle
+				 angle_roll = angle_roll * 0.9996 + angle_roll_acc * 0.0004;        //Correct the drift of the gyro roll angle with the accelerometer roll angle
+			}
+			else{                                                                //At first start
+				 angle_pitch = angle_pitch_acc;                                     //Set the gyro pitch angle equal to the accelerometer pitch angle
+				 angle_roll = angle_roll_acc;                                       //Set the gyro roll angle equal to the accelerometer roll angle
+				 set_gyro_angles = true;                                            //Set the IMU started flag
+			}
 			
 			#endif //PRINT_RAW_DATA			
 		#endif //GYRO
-		//while(micros()-timer1<4000);
-		//timer1=micros();
-		
 	}
 	return 0;
 }
