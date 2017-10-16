5013cae5884ac913c84de07511e5d3addef783ea
rafael2904 <rafa.grigorian@gmail.com>
2017-10-16 18:09:54 +0400

    clean

--- drv_/main.c
+++ drv_/main.c
@@ -4,14 +4,14 @@
 This code is written for 2-axis gimbal, but 3rd axis driver exist,
 if you want to turn z-axis on see function.c file PWM_update_3() function.
 
-This software drives 3 axis brushless gimbal using DRV8313 drivers,
+This software drives 3 axis brushless gimbal using DRV8313 drivers(by Texas Instruments),
 and Atmel AVR Atmega2560 microcontroller.
 This software uses Kalman filter for data filtering of mpu6050 accelerometer
-and gyroscope using i2c(TWI) interface.
+and gyroscope using i2c(TWI) interface for communication.
 For controlling brushles DC motors software uses sinusoidal tables,
-this can be generated on start of the program, or precalculated in microsoft excel,
-or other software, see function.c file.
-The code also uses PID controller for each axis.	
+this can be generated at the beginning of the program, or precalculated in
+microsoft excel or other software. See function.c file.
+The code also uses PID controller for regulation of each axis speed.	
 */
 #include "Variable.h"
 #include "defines.h"
